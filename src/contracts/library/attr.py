d82f5dea8f46406fbee57e8d1e5085d8a69e2e42
Andrea Censi <andrea@cds.caltech.edu>
2012-07-23 13:16:23 -0700

    some test cases

--- src/contracts/library/attr.py
+++ src/contracts/library/attr.py
@@ -1,35 +1,30 @@
 from ..interface import Contract, ContractNotRespected
-from ..syntax import (W, contract_expression, O, add_contract,
-        add_keyword,
-    Keyword)
-from pyparsing import (Dict, delimitedList, Group, alphanums,
-        Suppress, Literal, Word)
+from ..syntax import W, contract_expression, add_contract, add_keyword, Keyword
+from pyparsing import (Dict, delimitedList, Group, alphanums, Suppress, Literal,
+    Word)
 
 
 class Attr(Contract):
-    def __init__(self, attrs=None, where=None):
+    def __init__(self, attrs, where=None):
+        # attrs: dict(str: contract)
         Contract.__init__(self, where)
+        if not attrs:
+            raise ValueError('Need some attributes') 
         self.attrs = attrs
 
     def check_contract(self, context, value):
-        if self.attrs is not None:
-            for k in self.attrs:
-                if hasattr(value, k):
-                    self.attrs[k]._check_contract(context, getattr(value, k))
-                else:
-                    error = 'Expected an attribute %r.' % k
-                    raise ContractNotRespected(contract=self, error=error,
-                                               value=value, context=context)
+        for k in self.attrs:
+            if hasattr(value, k):
+                self.attrs[k]._check_contract(context, getattr(value, k))
+            else:
+                error = 'Expected an attribute %r.' % k
+                raise ContractNotRespected(contract=self, error=error,
+                                           value=value, context=context)
 
 
     def __str__(self):
-        s = 'attr'
-        if self.attrs is not None:
-            for k in self.attrs:
-                s += '(%s:%s),' % (k, self.attrs[k])
-            if len(self.attrs.keys()):
-                s = s[:-1]  # chop off last ','
-        return s
+        sattrs = ";".join(['%s:%s' % (k, v) for (k, v) in self.attrs.items()]) 
+        return 'attr(%s)' % sattrs
 
     def __repr__(self):
         return 'Attr(%r)' % self.attrs
@@ -38,10 +33,9 @@ class Attr(Contract):
     def parse_action(s, loc, tokens):
         where = W(s, loc)
         attrs = tokens.get('attrs', None)
-        if attrs is not None:
-            # Python 3 only
-            # attrs = {k: v for k, v in attrs.items()}
-            attrs = dict([(k, v) for k, v in attrs.items()])
+        # Python 3 only
+        # attrs = {k: v for k, v in attrs.items()}
+        attrs = dict([(k, v) for k, v in attrs.items()])
         return Attr(attrs, where=where)
 
 attr_spec = Dict(
@@ -54,7 +48,7 @@ attr_spec = Dict(
 attrs_spec = ('(' - attr_spec - ')')
 
 
-attr_contract = Keyword('attr') + O(attrs_spec)
+attr_contract = Keyword('attr') - attrs_spec
 attr_contract.setParseAction(Attr.parse_action)
 
 add_contract(attr_contract)
