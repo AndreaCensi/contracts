7103075c6bf6158824c5f62f38123d721aef67be
Andrea Censi <andrea@cds.caltech.edu>
2012-08-14 21:05:01 -0700

    allow np.number types in comparisons

--- src/contracts/__init__.py
+++ src/contracts/__init__.py
@@ -10,10 +10,20 @@ contract_expression = syntax.contract_expression
 from .interface import (Contract, ContractNotRespected,
                         ContractSyntaxError, ContractException)
 
-from .main import (check, fail, check_multiple, contract_decorator as contract,
-                    new_contract,
+from .main import (check, fail, check_multiple, contract_decorator,
                     contracts_decorate as decorate,
                     parse_flexible_spec as parse)
+
+# Just make them appear as belonging to the "contracts" Module
+# So that Eclipse and other IDEs will not get confused.
+def contract(*args, **kwargs):
+    return contract_decorator(*args, **kwargs)
+contract.__doc__ = contract_decorator.__doc__
+
+from .main import new_contract as new_contract_main
+def new_contract(*args, **kwargs):
+    return new_contract_main(*args, **kwargs)
+new_contract.__doc__ = new_contract_main.__doc__
 
 from .enabling import disable_all, enable_all, all_disabled
 
