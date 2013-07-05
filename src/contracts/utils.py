00a3575b5a0793269c54aa73f432008dfa2c54bc
Andrea Censi <andrea@cds.caltech.edu>
2013-07-05 08:58:01 +0200

    cleaner

--- src/contracts/utils.py
+++ src/contracts/utils.py
@@ -1,5 +1,5 @@
 
 def indent(s, prefix):
     lines = s.split('\n')
-    lines = ['%s%s' % (prefix, line) for line in lines]
+    lines = ['%s%s' % (prefix, line.rstrip()) for line in lines]
     return '\n'.join(lines)
