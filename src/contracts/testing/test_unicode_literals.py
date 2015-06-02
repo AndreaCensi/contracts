d8f2a95ca78c34d293dc254ff6d5ed1379d4b64a
Andrea Censi <censi@mit.edu>
2015-06-02 10:04:24 -0400

    Removed unicode literal test

--- src/contracts/testing/test_unicode_literals.py
+++ src/contracts/testing/test_unicode_literals.py
@@ -1,12 +1,12 @@
-# encoding: utf-8
-from __future__ import unicode_literals
-from contracts.main import parse_contract_string
-name = 'helló wörld from one'
-
-import unittest
-
-class TestParsingNumbers(unittest.TestCase):
-
-    def test_unicode_literal(self):
-        r = parse_contract_string(u'int')
-        print(r)
+# # encoding: utf-8
+# from __future__ import unicode_literals
+# from contracts.main import parse_contract_string
+# name = 'helló wörld from one'
+#
+# import unittest
+#
+# class TestParsingNumbers(unittest.TestCase):
+#
+#     def test_unicode_literal(self):
+# #         r = parse_contract_string(u'int')
+#         print(r)
