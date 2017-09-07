d1cc3df943efb202d8abaa6f6e90b0c1faf5258d
Andrea Censi <acensi@ethz.ch>
2017-09-08 00:02:47 +0200

    Removing basicConfig()

--- src/contracts/__init__.py
+++ src/contracts/__init__.py
@@ -2,7 +2,7 @@ __version__ = '1.7.15'
 
 import logging
 
-logging.basicConfig()
+#logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 from .interface import (Contract, ContractNotRespected,
