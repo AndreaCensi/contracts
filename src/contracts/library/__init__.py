b31c44b3f96503cbf3d6821dae07112e857ffee0
Slorg1 <slorg1@gmail.com>
2018-05-08 15:54:48 -0400

    + add datetime_tz contract to check for datetime with timezones

--- src/contracts/library/__init__.py
+++ src/contracts/library/__init__.py
@@ -1,25 +1,30 @@
-from .suggester import create_suggester
-from .dummy import Any, Never
-from .separate_context import SeparateContext
-from .types_misc import Type, CheckType, Number
-from .strings import *
-from .lists import List
-from .seq import Seq
-from .tuple import Tuple
-from .dicts import Dict
-from .map import Map
-from .sets import *
-from .attributes import Attr
-from .files import File
-from .collection import Collection
-
-from .comparison import CheckOrder
 from .arithmetic import Binary, Unary
+from .attributes import Attr
+from .collection import Collection
+from .comparison import CheckOrder
 from .compositions import OR, And, Not, composite_contract, or_contract
+from .datetime_tz import DatetimeWithTz
+from .dicts import Dict
+from .dummy import Any, Never
+from .extensions import (identifier_expression, identifier_contract,
+                         Extension, CheckCallable)
+from .files import File
+from .isinstance_imp import *
+from .lists import List
+from .map import Map
+from .miscellaneous_aliases import *
+from .scoped_variables import scoped_variables_ref
+from .separate_context import SeparateContext
+from .seq import Seq
+from .sets import *
+from .simple_values import EqualTo, SimpleRValue
+from .strings import *
+from .suggester import create_suggester
+from .tuple import Tuple
+from .types_misc import Type, CheckType, Number
 from .variables import (BindVariable, VariableRef, misc_variables_contract,
                         int_variables_contract, misc_variables_ref,
                         int_variables_ref)
-from .simple_values import EqualTo, SimpleRValue
 
 try:
     import numpy
@@ -29,10 +34,3 @@ else:
     from .array import (ShapeContract, Shape, Array, ArrayConstraint, DType,
                         dtype, ArrayOR, ArrayAnd)
 
-from .extensions import (identifier_expression, identifier_contract,
-                         Extension, CheckCallable)
-
-from .isinstance_imp import *
-from .scoped_variables import scoped_variables_ref
-
-from .miscellaneous_aliases import *
---
+++ src/contracts/library/datetime_tz.py
@@ -0,0 +1,41 @@
+import datetime
+
+from ..interface import Contract, ContractNotRespected, describe_type
+from ..syntax import Keyword, W, add_contract, add_keyword
+
+
+class DatetimeWithTz(Contract):
+
+    def __init__(self, length_contract=None,
+                 elements_contract=None, where=None):
+        Contract.__init__(self, where)
+        self.length_contract = length_contract
+        self.elements_contract = elements_contract
+
+    def check_contract(self, context, value, silent):
+
+        if not isinstance(value, datetime.datetime):
+            error = 'Expected a datetime, got %r.' % describe_type(value)
+            raise ContractNotRespected(self, error, value, context)
+
+        if value.tzinfo is None:
+            error = 'Expected a datetime with a timezone not a naive datetime '
+            raise ContractNotRespected(self, error, value, context)
+
+    def __str__(self):
+        return 'datetime_tz'
+
+    __repr__ = __str__
+
+    @staticmethod
+    def parse_action(s, loc, tokens):
+        where = W(s, loc)
+        return DatetimeWithTz(where=where)
+
+
+list_contract = (Keyword('datetime_tz'))
+list_contract.setParseAction(DatetimeWithTz.parse_action)
+
+list_contract.setName('datetime_tz contract')
+add_keyword('datetime_tz')
+add_contract(list_contract)
