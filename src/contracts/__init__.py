f3459053904a61e94eb602b3dbdbba271be1671c
Slorg1 <slorg1@gmail.com>
2018-10-24 13:00:43 -0400

    + fix import

--- src/contracts/__init__.py
+++ src/contracts/__init__.py
@@ -2,21 +2,16 @@ __version__ = '1.8.6'
 
 import logging
 
-# logging.basicConfig()
+#logging.basicConfig()
 logger = logging.getLogger(__name__)
 
-from .enabling import disable_all, enable_all, all_disabled
 from .interface import (Contract, ContractNotRespected,
                         CannotDecorateClassmethods,
                         ContractSyntaxError, ContractException)
-from .interface import describe_value, describe_type, describe_value_multiline
+
 from .main import (check, fail, check_multiple, contract_decorator,
                    contracts_decorate as decorate,
                    parse_flexible_spec as parse)
-from .main import new_contract as new_contract_main
-from .metaclass import ContractsMeta, with_metaclass
-from .useful_contracts import *
-from .utils import *
 
 
 # Just make them appear as belonging to the "contracts" Module
@@ -24,8 +19,9 @@ from .utils import *
 def contract(*args, **kwargs):
     return contract_decorator(*args, **kwargs)
 
-
 contract.__doc__ = contract_decorator.__doc__
+
+from .main import new_contract as new_contract_main
 
 
 def new_contract(*args):
@@ -34,10 +30,17 @@ def new_contract(*args):
 
 new_contract.__doc__ = new_contract_main.__doc__
 
+from .enabling import disable_all, enable_all, all_disabled
+
 # A couple of useful functions
+from .interface import describe_value, describe_type, describe_value_multiline
+from .utils import *
+
+from .metaclass import ContractsMeta, with_metaclass
 
 ContractsMeta.__module__ = 'contracts'
 
 # And after everything else is loaded, load the  utils
+from .useful_contracts import *
 # After everything is loaded, load aliases
 # from .library import miscellaneous_aliases  # @UnusedImport
